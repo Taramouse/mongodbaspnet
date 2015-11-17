3ab4e5a665bdd691381cb7f4b651a67ebe7f8159
Taramouse <taramouse@live.co.uk>
2015-11-17 15:35:27 +0000

    Add Model. Added Employee.cs.

---
+++ MongoDB.AspNet/MongoDB.AspNet/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MongoDB.AspNet.Models
+{
+    public class Employee
+    {
+        [BsonElement("_id")]
+        public ObjectId Id { get; set; }
+        [BsonElement("name")]
+        public ObjectId Name { get; set; }
+        [BsonElement("email")]
+        public ObjectId Email { get; set; }
+        [BsonElement("level")]
+        public ObjectId Level { get; set; }
+        [BsonElement("createddate")]
+        public ObjectId CreateDate { get; set; }
+
+        public override string ToString()
+        {
+            string str = string.Format("Id: {0}\r\nName: {1}\r\nEmail: {2}\r\nLevel: {3}\r\nCreatedDate: {4}\r\n\r\n",
+                Id, Name, Email, Level, CreateDate);
+            return str;
+        }
+    }
+}
